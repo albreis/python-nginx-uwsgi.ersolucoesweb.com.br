77aa54deea2162f51b1b597ceaa5f2584ddfbe60
everaldo <albreisdev@gmail.com>
2022-05-12 21:33:16 +0000

    Define as variáveis a serem usadas no template

--- main.py
+++ main.py
@@ -7,3 +7,4 @@ def application(env, start_response):
     page = 'home' if env['PATH_INFO'] == '/' else env['PATH_INFO']
     template_path = './templates/' + page + '.html'
     page_template = open(template_path if exists(template_path) else './templates/404.html', 'r')
+    templateVars = {"title": "Python + Nginx", "version": version, "page": page, "template_path": template_path}
