abd7f65cfd7927adec25f127067ce62c876ffaf9
everaldo <albreisdev@gmail.com>
2022-05-12 21:32:26 +0000

    Abre o template para leitura ou então usa o 404.html

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ def application(env, start_response):
     start_response("200 OK", [("Content-Type", "text/html")])
     page = 'home' if env['PATH_INFO'] == '/' else env['PATH_INFO']
     template_path = './templates/' + page + '.html'
+    page_template = open(template_path if exists(template_path) else './templates/404.html', 'r')
