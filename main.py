a4b5f5086bb63a8bed7c5a78f212df0fa1c55e67
everaldo <albreisdev@gmail.com>
2022-05-12 20:54:42 +0000

    Alterar content-type para text/html

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import sys
 
 def application(env, start_response):
     version = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
-    start_response("200 OK", [("Content-Type", "text/plain")])
-    message = "Hakuna Matata from a default Nginx uWSGI Python {} app in a Docker container (default)".format(
+    start_response("200 OK", [("Content-Type", "text/html")])
         version
     )
     return [message.encode("utf-8")]
