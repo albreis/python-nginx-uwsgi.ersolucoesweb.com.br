ecd5b72648f04d4abfaf2ee10d4ae9659a9db44c
everaldo <albreisdev@gmail.com>
2022-05-12 21:31:54 +0000

    Monta o caminho do template de pagina

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ def application(env, start_response):
     version = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
     start_response("200 OK", [("Content-Type", "text/html")])
     page = 'home' if env['PATH_INFO'] == '/' else env['PATH_INFO']
+    template_path = './templates/' + page + '.html'
