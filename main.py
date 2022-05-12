bd6d28a348fe7042f09f37ea1cf71e1564b757f6
everaldo <albreisdev@gmail.com>
2022-05-12 20:55:04 +0000

    Utilizr arquivo de template

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import sys
 def application(env, start_response):
     version = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
     start_response("200 OK", [("Content-Type", "text/html")])
+    home = open('./templates/home.html','r')
+    message = home.read().format(
         version
     )
     return [message.encode("utf-8")]
