a56d17adeb3332ee62bb3cd6cd8d9d9cf0933422
everaldo <albreisdev@gmail.com>
2022-05-12 21:31:35 +0000

    Captura o path da pagina atual

--- main.py
+++ main.py
@@ -4,8 +4,4 @@ import sys
 def application(env, start_response):
     version = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
     start_response("200 OK", [("Content-Type", "text/html")])
-    home = open('./templates/home.html','r')
-    message = home.read().format(
-        version
-    )
-    return [message.encode("utf-8")]
+    page = 'home' if env['PATH_INFO'] == '/' else env['PATH_INFO']
